7f2e4fc0575e447a9f46ecbe1759dc22b07ec1e9
rudhrapriyat <rudhrapriya98@gmail.com>
2024-12-07 19:15:09 -0800

    python text-analysis.py

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -12,6 +14,9 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Create client using endpoint and key
+        # Create client using endpoint and key
+credential = AzureKeyCredential(ai_key)
+ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
 
         # Analyze each text file in the reviews folder
